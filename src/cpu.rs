01d465740a808c1beecc5e610e6778e890ebb541
mp3 <im.in.a.coma.xxx@gmail.com>
2024-04-11 01:02:01 +0900

    refactor(cpu): Extract flag update logic to new method

--- src/cpu.rs
+++ src/cpu.rs
@@ -15,6 +15,30 @@ impl CPU {
     }
   }
 
+  fn lda(&mut self, value: u8) {
+    self.register_a = value;
+    self.update_zero_and_nagative_flags(self.register_a);
+  }
+
+  fn tax(&mut self) {
+    self.register_x = self.register_a;
+    self.update_zero_and_nagative_flags(self.register_x);
+  }
+
+  fn update_zero_and_nagative_flags(&mut self, result: u8) {
+    if result == 0 {
+      self.status = self.status | 0b0000_0010;
+    } else {
+      self.status = self.status & 0b1111_1101;
+    }
+
+    if result & 0b1000_0000 != 0 {
+      self.status = self.status | 0b1000_0000;
+    } else {
+      self.status = self.status & 0b0111_1111;
+    }
+  }
+
   pub fn interpret(&mut self, program: Vec<u8>) {
     self.program_counter = 0;
 
@@ -28,36 +52,11 @@ impl CPU {
           self.program_counter += 1;
           self.register_a = param;
 
-          if self.register_a == 0 {
-            self.status = self.status | 0b0000_0010;
-          } else {
-            self.status = self.status & 0b1111_1101;
-          }
-
-          if self.register_a & 0b1000_0000 != 0 {
-            self.status = self.status | 0b1000_0000;
-          } else {
-            self.status = self.status & 0b0111_1111;
-          }
+          self.lda(param);
         },
-        0xAA => {
-          self.register_x = self.register_a;
-
-          if self.register_x == 0 {
-            self.status = self.status | 0b0000_0010;
-          } else {
-            self.status = self.status & 0b1111_1101;
-          }
-
-          if self.register_x & 0b1000_0000 != 0 {
-            self.status = self.status | 0b1000_0000;
-          } else {
-            self.status = self.status & 0b0111_1111;
-          }
-        },
-        0x00 => {
-          return;
-        }
+        0xAA => self.tax(),
+        0x00 => return,
+        _ => todo!()
       }
     }
   }
