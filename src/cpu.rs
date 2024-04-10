ad7176f4eaa7bed1b7541cdf2d33f99357043c4a
mp3 <im.in.a.coma.xxx@gmail.com>
2024-04-10 23:10:18 +0900

    feat(cpu): Implement opcode 0xA9 in CPU module

--- src/cpu.rs
+++ src/cpu.rs
@@ -21,7 +21,23 @@ impl CPU {
       self.program_counter += 1;
 
       match opscode {
-        _ => todo!()
+        0xA9 => {
+          let param = program[self.program_counter as usize];
+          self.program_counter += 1;
+          self.register_a = param;
+
+          if self.register_a == 0 {
+            self.status = self.status | 0b0000_0010;
+          } else {
+            self.status = self.status & 0b1111_1101;
+          }
+
+          if self.register_a & 0b1000_0000 != 0 {
+            self.status = self.status | 0b1000_0000
+          } else {
+            self.status = self.status & 0b0111_1111;
+          }
+        }
       }
     }
   }
