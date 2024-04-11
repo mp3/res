71fafca5e9d2bcb1d4f01111ac9d0f11373b038c
mp3 <im.in.a.coma.xxx@gmail.com>
2024-04-11 13:01:42 +0900

    feat(cpu): Add STA instruction to CPU

--- src/cpu.rs
+++ src/cpu.rs
@@ -52,6 +52,11 @@ impl CPU {
     self.update_zero_and_nagative_flags(self.register_x);
   }
 
+  fn sta(&mut self, mode: &AddressingMode) {
+    let addr = self.get_operand_address(mode);
+    self.mem_write(addr, self.register_a);
+  }
+
   fn update_zero_and_nagative_flags(&mut self, result: u8) {
     if result == 0 {
       self.status = self.status | 0b0000_0010;
@@ -126,6 +131,14 @@ impl CPU {
         }
         0xAA => self.tax(),
         0xE8 => self.inx(),
+        0x85 => {
+          self.sta(&AddressingMode::ZeroPage);
+          self.program_counter += 1;
+        }
+        0x95 => {
+          self.sta(&AddressingMode::ZeroPage_X);
+          self.program_counter += 1;
+        }
         0x00 => return,
         _ => todo!()
       }
