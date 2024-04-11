158bc651da28192c64e9102118fe7e0ba66a38f0
mp3 <im.in.a.coma.xxx@gmail.com>
2024-04-11 13:03:17 +0900

    test(cpu): Update assertions in test_0xa9_lda_immediate_load_data

--- src/cpu.rs
+++ src/cpu.rs
@@ -202,8 +202,8 @@ mod test {
   fn test_0xa9_lda_immediate_load_data() {
     let mut cpu = CPU::new();
     cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
-    assert_eq!(cpu.register_a, 0x05);
-    assert!(cpu.status & 0b0000_0010 == 0b00);
+    assert_eq!(cpu.register_a, 5);
+    assert!(cpu.status & 0b0000_0010 == 0);
     assert!(cpu.status & 0b1000_0000 == 0);
   }
 
