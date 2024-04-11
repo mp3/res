8af4648730f4a7b9fb630ba1d1fe19d3f5ba6ff6
mp3 <im.in.a.coma.xxx@gmail.com>
2024-04-11 12:37:57 +0900

    feat(cpu): Add AddressingMode enum and get_operand_address function

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,9 +1,25 @@
 pub struct CPU {
   pub register_a: u8,
   pub register_x: u8,
+  pub register_y: u8,
   pub status: u8,
   pub program_counter: u16,
   memory: [u8; 0xffff],
+}
+
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+pub enum AddressingMode {
+  Immediate,
+  ZeroPage,
+  ZeroPage_X,
+  ZeroPage_Y,
+  Absolute,
+  Absolute_X,
+  Absolute_Y,
+  Indirect_X,
+  Indirect_Y,
+  NoneAddressing,
 }
 
 impl CPU {
@@ -11,6 +27,7 @@ impl CPU {
     CPU {
       register_a: 0,
       register_x: 0,
+      register_y: 0,
       status: 0,
       program_counter: 0,
       memory: [0; 0xffff],
@@ -126,6 +143,54 @@ impl CPU {
         0xE8 => self.inx(),
         0x00 => return,
         _ => todo!()
+      }
+    }
+  }
+
+  fn get_operand_address(&self, mode: &AddressingMode) -> u16 {
+    match mode {
+      AddressingMode::Immediate => self.program_counter,
+      AddressingMode::ZeroPage => self.mem_read(self.program_counter) as u16,
+      AddressingMode::Absolute => self.mem_read_u16(self.program_counter),
+      AddressingMode::ZeroPage_X => {
+        let pos = self.mem_read(self.program_counter);
+        let addr = pos.wrapping_add(self.register_x) as u16;
+        addr
+      }
+      AddressingMode::ZeroPage_Y => {
+        let pos = self.mem_read(self.program_counter);
+        let addr = pos.wrapping_add(self.register_y) as u16;
+        addr
+      }
+      AddressingMode::Absolute_X => {
+        let base = self.mem_read_u16(self.program_counter);
+        let addr = base.wrapping_add(self.register_x) as u16;
+        addr
+      }
+      AddressingMode::Absolute_Y => {
+        let base = self.mem_read_u16(self.program_counter);
+        let addr = base.wrapping_add(self.register_y) as u16;
+        addr
+      }
+      AddressingMode::Indirect_X => {
+        let base = self.mem_read(self.program_counter);
+
+        let ptr: u8 = base.wrapping_add(self.register_x);
+        let lo = self.mem_read(ptr as u16);
+        let hi = self.mem_read(ptr.wrapping_add(1) as u16);
+        (hi as u16) << 8 | (lo as u16)
+      }
+      AddressingMode::Indirect_Y => {
+        let base = self.mem_read(self.program_counter);
+
+        let lo = self.mem_read(base as u16);
+        let hi = self.mem_read((base as u8).wrapping_add(1) as u16);
+        let deref_base = (hi as u16) << 8 | (lo as u16);
+        let deref = deref_base.wrapping_add(self.register_y as u16);
+        deref
+      }
+      AddressingMode::NoneAddressing => {
+        panic!("mode {:?} is not supported", mode)
       }
     }
   }
