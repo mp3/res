75d5072827c6db43827cec80afd1356826f8ffc0
mp3 <im.in.a.coma.xxx@gmail.com>
2024-04-15 22:11:04 +0900

    feat(cpu): Add sbc function and opcode handler

--- src/cpu.rs
+++ src/cpu.rs
@@ -150,6 +150,12 @@ impl CPU {
     self.mem_write(addr, self.register_a);
   }
 
+  fn sbc(&mut self, mode: &AddressingMode) {
+    let addr = self.get_operand_address(mode);
+    let data = self.mem_read(addr);
+    self.add_to_refister_a(((data as i8).wrapping_neg().wrapping_sub(1)) as u8);
+  }
+
   fn adc(&mut self, mode: &AddressingMode) {
     let addr = self.get_operand_address(mode);
     let value = self.mem_read(addr);
@@ -232,6 +238,9 @@ impl CPU {
         0x00 => return,
         0x69 | 0x65 | 0x75 | 0x6d | 0x7d | 0x79 | 0x61 | 0x71 => {
           self.adc(&opcode.mode);
+        },
+        0xe9 | 0xe5 | 0xf5 | 0xed | 0xfd | 0xf9 | 0xe1 | 0xf1 => {
+          self.sbc(&opcode.mode);
         },
         _ => todo!(),
       }
