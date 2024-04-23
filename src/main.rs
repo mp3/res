b5e847229627782225c72df11985815b04d3aa66
mp3 <im.in.a.coma.xxx@gmail.com>
2024-04-23 23:48:04 +0900

    feat(cpu): Make Mem trait public, add user input handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod cpu;
 pub mod opcodes;
+use cpu::Mem;
 use cpu::CPU;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -12,6 +13,29 @@ extern crate lazy_static;
 
 #[macro_use]
 extern crate bitflags;
+
+fn handle_user_input(cpu: &mut CPU, event_pump: &mut EventPump) {
+    for event in event_pump.poll_iter() {
+        match event {
+            Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                std::process::exit(0)
+            },
+            Event::KeyDown { keycode: Some(Keycode::W), .. } => {
+                cpu.mem_write(0xff, 0x77);
+            },
+            Event::KeyDown { keycode: Some(Keycode::S), .. } => {
+                cpu.mem_write(0xff, 0x73);
+            },
+            Event::KeyDown { keycode: Some(Keycode::A), .. } => {
+                cpu.mem_write(0xff, 0x61);
+            },
+            Event::KeyDown { keycode: Some(Keycode::D), .. } => {
+                cpu.mem_write(0xff, 0x64);
+            },
+            _ => {/* do nothing */}
+        }
+    }
+}
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
