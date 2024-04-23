45d6d3dda894afb1d52bd6b89ca66f538e5cd541
mp3 <im.in.a.coma.xxx@gmail.com>
2024-04-23 23:39:42 +0900

    feat(main.rs): Add SDL2 initialization and window creation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 pub mod cpu;
 pub mod opcodes;
 use cpu::CPU;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::EventPump;
 
 #[macro_use]
 extern crate lazy_static;
@@ -9,6 +14,18 @@ extern crate lazy_static;
 extern crate bitflags;
 
 fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("NES Emulator", (32.0 * 10.0) as u32, (32.0 * 10.0) as u32)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas.set_scale(10.0, 10.0).unwrap();
+
     let game_code = vec![
         0x20, 0x06, 0x06, 0x20, 0x38, 0x06, 0x20, 0x0d, 0x06, 0x20, 0x2a, 0x06, 0x60, 0xa9, 0x02,
         0x85, 0x02, 0xa9, 0x04, 0x85, 0x03, 0xa9, 0x11, 0x85, 0x10, 0xa9, 0x10, 0x85, 0x12, 0xa9,
